ab2c725c724c8420508f2736186221dcdb51148e
Zachery Coleman <coleman.zachery@gmail.com>
2025-11-13 22:45:56 -0500

    .

--- libs/generators.py
+++ libs/generators.py
@@ -3,23 +3,23 @@ from libs.helpers import shuffle_hand, cardinal_subsets
 import torch
 
 def generate_royal_flush(card_exceptions=[]):
-    ranks = RANKS[-5:]
-    exclude_suits = set()
-    for card_exception in card_exceptions:
-        rank_char, suit_char = card_exception
-        if rank_char in ranks:
-            exclude_suits.add(suit_char)
-    eligible_suits = [suit for suit in SUITS[3:] if suit not in exclude_suits]
-    if len(eligible_suits) == 0: return None
-    suit = eligible_suits[torch.randint(len(eligible_suits)).item()]
-    hand = [rank + suit for rank in ranks]
+    eligible_hands = [[rank + suit for rank in RANKS[-5:]] for suit in SUITS[-4:]]
+    for rank, suit in card_exceptions:
+        eligible_hands = [hand for hand in eligible_hands if rank + suit not in hand]
+    hand = eligible_hands[torch.randint(len(eligible_hands)).item()]
     return shuffle_hand(hand)
 
 def generate_straight_flush(card_exceptions=[]):
-    eligible_hands = {suit: [cardinal_subsets(RANKS[3:-1], 5)] for suit in SUITS[-4:]}
+    eligible_hands = [[[rank + suit for rank in subset] for subset in cardinal_subsets(RANKS[3:-1], 5)] for suit in SUITS[-4:]]
+    for rank, suit in card_exceptions:
+        eligible_hands = [hand for suit_hands in eligible_hands for hand in suit_hands if rank + suit not in hand]
+    hand = eligible_hands[torch.randint(len(eligible_hands)).item()]
+    return shuffle_hand(hand)
+
+def generate_four_of_a_kind(card_exceptions=[]):
+    eligible_hands = [rank + suit for suit in SUITS[-4:] for rank in RANKS[3:]]
     return eligible_hands
 
-def generate_four_of_a_kind(): pass
 def generate_full_house(): pass
 def generate_flush(): pass
 def generate_straight(): pass
@@ -29,4 +29,3 @@ def generate_high_card(): pass
 
 def generate_nothing():
     return ['__', '__', '__', '__', '__']
-
