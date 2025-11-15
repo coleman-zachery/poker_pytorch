6d43dade1b3b2cf6d44681d5d0a3fb91a518fa4d
Zachery Coleman <coleman.zachery@gmail.com>
2025-11-14 19:20:02 -0500

    .

--- libs/helpers.py
+++ libs/helpers.py
@@ -2,6 +2,9 @@
 from libs.constants import RANKS, SUITS
 import torch
 
+def cardinal_subsets(x, n):
+    return list(zip(*(x[i:] for i in range(n))))
+
 def card_str_to_tuple(card_str):
     rank_char, suit_char = card_str
     rank = RANKS.index(rank_char)
@@ -13,20 +16,39 @@ def card_strings_to_tensor(card_strings):
     card_tensor = torch.tensor(card_tuples, dtype=torch.long)
     return card_tensor
 
+def is_sequence(n, _numbers, wilds):
+    numbers = set(numbers)
+    if not numbers: return wilds >= n
+
+
+    _rank_tuples = [i for i, _ in enumerate(RANKS) if i > 2]
+    rank_tuples = [_rank_tuples[-1]] + _rank_tuples
+
+    for subset in cardinal_subsets(rank_tuples):
+        
+
+
+
 def classify_poker_hand(card_tuples):
-    ranks = [rank for rank, _ in card_tuples if rank > 2]
-    rank_counts = {rank: ranks.count(rank) for rank in set(ranks)}
-    wild_ranks = [rank for rank, _ in card_tuples if rank == 2]
-    is_flush = len(set(suit for _, suit in card_tuples if suit > 1)) == 1
-    sorted_ranks = sorted(ranks)
+    ranks = []
+    rank_counts = {}
+    wild_ranks = 0
+    for rank, _ in card_tuples:
+        if rank > 2:
+            ranks.append(rank)
+            rank_counts[rank] = rank_counts.get(rank, 0) + 1
+        if rank == 2:
+            wild_ranks += 1
+
     # check for straight (including low-Ace straight)
     is_straight = False
     if len(sorted_ranks) >= 5:
-        print(sorted_ranks, list(range(sorted_ranks[0], sorted_ranks[0] + 5)))
         if sorted_ranks == list(range(sorted_ranks[0], sorted_ranks[0] + 5)):
             is_straight = True
         elif sorted_ranks[-4:] == [3, 4, 5, 6] and sorted_ranks[0] == 15:
             is_straight = True
+
+    is_flush = len(set(suit for _, suit in card_tuples if suit > 1)) == 1
 
     if is_straight and is_flush and sorted_ranks[-1] == 15:
         return "royal flush"
@@ -87,9 +109,6 @@ def shuffle_hand(hand):
     indices = torch.randperm(len(hand))
     shuffled = [hand[i] for i in indices]
     return shuffled
-
-def cardinal_subsets(x, n):
-    return list(zip(*(x[i:] for i in range(n))))
 
 def random_item(items):
     return items[torch.randint(len(items), (1,)).item()]
