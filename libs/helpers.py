5b1ab62f02cdb6db9533f22692ea91994f207161
Zachery Coleman <coleman.zachery@gmail.com>
2025-11-14 23:47:34 -0500

    .

--- libs/helpers.py
+++ libs/helpers.py
@@ -16,61 +16,53 @@ def card_strings_to_tensor(card_strings):
     card_tensor = torch.tensor(card_tuples, dtype=torch.long)
     return card_tensor
 
-def is_sequence(n, _numbers, wilds):
+def find_sequence(size, _numbers, num_wilds):
     numbers = set(_numbers)
-    if not numbers: return wilds >= n
-
+    if not numbers: return num_wilds >= size
     _rank_tuples = [i for i, _ in enumerate(RANKS) if i > 2]
-    rank_tuples = [_rank_tuples[-1]] + _rank_tuples
-
-    print(rank_tuples)
-    for subset in cardinal_subsets(rank_tuples, n):
-        if wilds >= len(set(subset) - numbers): return True
-    return False
+    rank_tuples = [_rank_tuples[-1]] + _rank_tuples # allow wrapping high card to low-side
+    for subset in cardinal_subsets(rank_tuples, size)[::-1]:
+        if num_wilds >= len(set(subset) - numbers): return subset
+    return None
 
 def classify_poker_hand(card_tuples):
     ranks = []
     rank_counts = {}
     wild_ranks = 0
-    for rank, _ in card_tuples:
+    suit_counts = {}
+    wild_suits = 0
+    for rank, suit in card_tuples:
         if rank > 2:
             ranks.append(rank)
             rank_counts[rank] = rank_counts.get(rank, 0) + 1
-        if rank == 2:
-            wild_ranks += 1
+        if rank == 2: wild_ranks += 1
+        if suit > 2: suit_counts[suit] = suit_counts.get(suit, 0) + 1
+        if suit == 2: wild_suits += 1
 
-    # check for straight (including low-Ace straight)
-    is_straight = False
-    if len(sorted_ranks) >= 5:
-        if sorted_ranks == list(range(sorted_ranks[0], sorted_ranks[0] + 5)):
-            is_straight = True
-        elif sorted_ranks[-4:] == [3, 4, 5, 6] and sorted_ranks[0] == 15:
-            is_straight = True
+    _sorted_rank_counts = sorted(rank_counts.items(), key=lambda x: x[0], reverse=True)
+    sorted_ranks, sorted_rank_counts = sorted(_sorted_rank_counts, key=lambda x: x[1], reverse=True)
+    if sorted_rank_counts[0] + wild_ranks >= 5: return "five of a kind", [sorted_ranks[0]]
 
-    is_flush = len(set(suit for _, suit in card_tuples if suit > 1)) == 1
+    sequence = find_sequence(5, ranks, wild_ranks)
+    is_flush = sorted(suit_counts.values(), reverse=True)[0] + wild_suits >= 5
+    if sequence == list(range(11, 16)) and is_flush: return "royal flush", [max(sequence)]
+    if sequence and is_flush: return "straight flush", sequence
 
-    if is_straight and is_flush and sorted_ranks[-1] == 15:
-        return "royal flush"
-    elif is_straight and is_flush:
-        return "straight flush"
-    elif 4 in rank_counts.values():
-        return "four of a kind"
-    elif 3 in rank_counts.values() and 2 in rank_counts.values():
-        return "full house"
-    elif is_flush:
-        return "flush"
-    elif is_straight:
-        return "straight"
-    elif 3 in rank_counts.values():
-        return "three of a kind"
-    elif list(rank_counts.values()).count(2) == 2:
-        return "two pair"
-    elif 2 in rank_counts.values():
-        return "one pair"
-    elif len(ranks) > 0:
-        return "high card"
-    else:
-        return "nothing"
+    if sorted_rank_counts[0] + wild_ranks == 4: return "four of a kind", [sorted_ranks[0]] * 4 + [sorted_ranks[1]]
+
+    kind_2 = sorted_rank_counts[1] + wild_ranks - 2
+    if kind_2 >= 0 and sorted_rank_counts[0] + kind_2 >= 3: return "full house", [sorted_ranks[0]] * 3 + [sorted_ranks[1]] * 2
+
+    if is_flush: return "flush", [RANKS[-1]] * wild_ranks + sorted_ranks
+    if sequence: return "straight", sequence
+
+    if sorted_rank_counts[0] + wild_ranks >= 3: return "three of a kind", [sorted_ranks[0]] * 3 + sorted_ranks[1:3]
+    kind_2 = sorted_rank_counts[1] + wild_ranks - 2
+    if kind_2 >= 0 and sorted_rank_counts[0] + kind_2 >= 2: return "two pair", [sorted_ranks[0]] * 2 + [sorted_ranks[1]] * 2 + [sorted_ranks[2]]
+    if sorted_rank_counts[0] + wild_ranks >= 2: return "one pair", [sorted_ranks[0]] * 2 + sorted_ranks[1:5]
+
+    _ranks = sorted_ranks + [0] * (5 - len(ranks))
+    return "high card", _ranks if _ranks[0] > 0 else "nothing", _ranks
 
 def poker_hand_label_to_index(label):
     label_map = {
@@ -85,6 +77,7 @@ def poker_hand_label_to_index(label):
         "four of a kind": 8,
         "straight flush": 9,
         "royal flush": 10,
+        "five of a kind": 11,
     }
     return label_map[label]
 
@@ -101,6 +94,7 @@ def reverse_poker_hand_index(index):
         8: "four of a kind",
         9: "straight flush",
         10: "royal flush",
+        11: "five of a kind",
     }
     return index_map[index]
 
