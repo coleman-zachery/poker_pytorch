f6be0198c3fa9483fe1213092b676eaa9d7e89e8
Zachery Coleman <coleman.zachery@gmail.com>
2025-11-15 23:29:35 -0500

    .

--- libs/helpers.py
+++ libs/helpers.py
@@ -39,8 +39,8 @@ def classify_poker_hand(card_tuples):
         if suit == 2: wild_suits += 1
 
     _sorted_rank_counts = sorted(rank_counts.items(), key=lambda x: x[0], reverse=True)
-    print(_sorted_rank_counts)
     _sorted_ranks, sorted_rank_counts = zip(*sorted(_sorted_rank_counts, key=lambda x: x[1], reverse=True))
+    sorted_rank_counts += [0] * (2 - len(sorted_rank_counts))
     sorted_ranks = list(_sorted_ranks)
     if sorted_rank_counts[0] + wild_ranks >= 5: return "five of a kind", [sorted_ranks[0]] * 5
 
@@ -50,6 +50,7 @@ def classify_poker_hand(card_tuples):
     if sequence and is_flush: return "straight flush", sequence
 
     if sorted_rank_counts[0] + wild_ranks == 4: return "four of a kind", [sorted_ranks[0]] * 4 + [sorted_ranks[1]]
+
 
     kind_2 = sorted_rank_counts[1] + wild_ranks - 2
     if kind_2 >= 0 and sorted_rank_counts[0] + kind_2 >= 3: return "full house", [sorted_ranks[0]] * 3 + [sorted_ranks[1]] * 2
