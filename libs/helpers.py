1bb521ea3d11857766fbe491dd28f7d7d48c628c
Zachery Coleman <coleman.zachery@gmail.com>
2025-11-29 15:49:58 -0500

    .

--- libs/helpers.py
+++ libs/helpers.py
@@ -36,30 +36,37 @@ def classify_poker_hand(poker_hand):
     for rank, suit in poker_hand:
         CARD_MAP[RANK_CHARS.index(rank)][SUIT_CHARS.index(suit)] += 1
 
-    wild_ranks = sum(CARD_MAP[1])
-    max_requirement = 5 - wild_ranks
+    _, RANKLESS_WILD, *RANKLESS_SUITS = CARD_MAP[0]
+    SUITLESS_WILD, WILD_WILD, *WILD_SUITS = CARD_MAP[1]
 
     # five of a kind (cache other kinds)
     rank_counts = {}
+    print("rank_counts")
+    print(rank_counts)
+    print()
+
+    suitless_requirement = 5 - sum([SUITLESS_WILD, WILD_WILD, sum(WILD_SUITS)])
     for i, row in list(enumerate(CARD_MAP))[:1:-1]:
-        if (row_sum := sum(row)) >= max_requirement:
+        if (row_sum := sum(row)) >= suitless_requirement:
             return "five of a kind", [i] * 5
         rank_counts[row_sum] = rank_counts.get(row_sum, []) + [i]
-    #    sorted_ranks = [rank for rank in sort_poker_hand(poker_hand) if rank != i]
-    #return "four of a kind", [i] * 4 + [sorted_ranks[0]]
 
     # royal and straight flush (cache straights and flushes)
+    _requirement = 5 - WILD_WILD
+    suitless_straight = None
     for i, subset in enumerate(cardinal_subsets(CARD_MAP[:1:-1] + CARD_MAP[-1], 5)):
-        wild_column = [rank[1] for rank in subset]
-        for col_i in range(2, 6):
-            requirement = _requirement - CARD_MAP[1][col_i]
-            suit_column = [rank[col_i] for rank in subset]
-            if sum([a + b > 0 for a, b in zip(wild_column, suit_column)]) >= requirement:
-                sequence = [((12 - i - j) % 13) + 2 for j in range(5)]
-                return ("royal flush" if i == 0 else "straight flush"), sequence
+        _, wilds, *suits = list(zip(*subset))
+        for j, suit in enumerate(suits):
+            sequence_count = sum([a + b > 0 for a, b in zip(wilds, suit)])
+            requirement = _requirement - WILD_SUITS[j]
+            if sequence_count >= requirement:
+                return "royal or straight flush"
+        sequence_count = sum([sum(rank) > 0 for rank in subset])
+        if suitless_straight is None and sequence_count >= suitless_requirement:
+            suitless_straight = "straight"
 
 #   		0	1		2	3	4	5
-#   		_	W		1	2	3	4
+#   		_	W		♣	♦	♥	♠
 #   0	_	0	0		0	0	0	0
 #   1	W	0	0		0	0	0	0
 #   								
@@ -77,22 +84,40 @@ def classify_poker_hand(poker_hand):
 #   13	K	0	0		0	0	0	0
 #   14	A	0	0		0	0	0	0
 
+    # four of a kind
+    #    sorted_ranks = [rank for rank in sort_poker_hand(poker_hand) if rank != i]
+    #return "four of a kind", [i] * 4 + [sorted_ranks[0]]
 
-
-
+    # full house
     kind_2 = sorted_rank_counts[1] + wild_ranks - 2
     if kind_2 >= 0 and sorted_rank_counts[0] + kind_2 >= 3: return "full house", [sorted_ranks[0]] * 3 + [sorted_ranks[1]] * 2
 
-    if is_flush: return "flush", [RANKS[-1]] * wild_ranks + sorted_ranks
-    if sequence: return "straight", sequence
+    # flush
+    flush_requirement = _requirement - RANKLESS_WILD
+    _, _, *suits = list(zip(*CARD_MAP[2:]))
+    for i, suit in enumerate(suits):
+        suit_count = sum(suit) + RANKLESS_SUITS[i] + WILD_SUITS[i]
+        if suit_count >= flush_requirement:
+            return "flush", None
 
+    # straight
+    if suitless_straight: return suitless_straight
+
+    # three of a kind
     if sorted_rank_counts[0] + wild_ranks >= 3: return "three of a kind", [sorted_ranks[0]] * 3 + sorted_ranks[1:3]
+
+    # two pair
     kind_2 = sorted_rank_counts[1] + wild_ranks - 2
     if kind_2 >= 0 and sorted_rank_counts[0] + kind_2 >= 2: return "two pair", [sorted_ranks[0]] * 2 + [sorted_ranks[1]] * 2 + [sorted_ranks[2]]
+
+    # one pair
     if sorted_rank_counts[0] + wild_ranks >= 2: return "one pair", [sorted_ranks[0]] * 2 + sorted_ranks[1:5]
 
+    # high card
     _ranks = sorted_ranks + [0] * (5 - len(ranks))
     if _ranks[0] > 0: return "high card", _ranks 
+
+    # nothing
     return "nothing", _ranks
 
 def poker_hand_label_to_index(label):
